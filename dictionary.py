da521a912b92e7c4d14b0f1e9d58c8883ca3d0ce
Erica Naglik <erica2605@live.com>
2018-10-27 23:56:11 -0700

    added comments

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,7 @@ import random
 from sys import argv
 i = 0
 user_input = int(argv[1])
+//choose a random word from words.txt
 def load_word():
     f = open('words.txt', 'r')
     words_list = f.readlines()
@@ -11,6 +12,7 @@ def load_word():
     secret_word = random.choice(split_words_list)
     return secret_word
 
+//prints random words as many times as user asks
 while i < user_input:
     print(load_word())
     i += 1
