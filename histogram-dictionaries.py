9cf4b2fcd3533021d2bf2950be20e2b8ecf77b7b
Erica Naglik <erica2605@live.com>
2018-10-30 10:36:46 -0700

    turned program into a function

--- histogram-dictionaries.py
+++ histogram-dictionaries.py
@@ -1,21 +1,25 @@
 import re
 import string
-frequency = {}
-first_list = []
-second_list = []
-document_text = open('test.txt', 'r')
-text_string = document_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-for word in match_pattern:
-    count = frequency.get(word,0)
-    frequency[word] = count + 1
-    first_list.append(word)
-for word in match_pattern:
-    second_list.append(int(frequency[word]))
+def histogram(text):
+    frequency = {}
+    first_list = []
+    second_list = []
+    document_text = open(text, 'r')
+    text_string = document_text.read().lower()
+    match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-zipped = zip(first_list, second_list)
+    for word in match_pattern:
+        count = frequency.get(word,0)
+        frequency[word] = count + 1
+        first_list.append(word)
+    for word in match_pattern:
+        second_list.append(int(frequency[word]))
 
-frequencies = list(set(zipped))
+    zipped = zip(first_list, second_list)
 
-print(dict(frequencies))
+    frequencies = list(set(zipped))
+
+    print(dict(frequencies))
+
+histogram('test.txt')
--- histogram-tuples.py
+++ histogram-tuples.py
@@ -1,63 +1,23 @@
 import re
 import string
-frequency = {}
-first_list = []
-second_list = []
-document_text = open('test.txt', 'r')
-text_string = document_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-for word in match_pattern:
-    count = frequency.get(word,0)
-    frequency[word] = count + 1
-    first_list.append(word)
-for word in match_pattern:
-    second_list.append(int(frequency[word]))
+def histogram(text):
+    frequency = {}
+    first_list = []
+    second_list = []
+    document_text = open(text, 'r')
+    text_string = document_text.read().lower()
+    match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-zipped = zip(first_list, second_list)
+    for word in match_pattern:
+        count = frequency.get(word,0)
+        frequency[word] = count + 1
+        first_list.append(word)
+    for word in match_pattern:
+        second_list.append(int(frequency[word]))
 
-please = list(set(zipped))
+    zipped = zip(first_list, second_list)
 
-print(dict(please))
+    print(list(set(zipped)))
 
-
-
-
-
-
-
-
-
-
-
-
-# def open_file():
-#
-#     f = open('source_text.txt', 'r')
-#     words_list = f.readlines()
-#     f.close()
-#
-#     split_words_list = words_list[0].split(' ')
-#
-# def histogram():
-#
-#     for i in range (len(split_words_list) -1):
-#         word_count = 1
-#         if split_words_list[i] == split_words_list[i+1]:
-#             word_count += 1
-#         else:
-#             word_count = 1
-#
-#     return str(i) and str(word_count)
-#
-# def run_histogram():
-#     histogram = []
-#
-#     open_file()
-#
-#     while source_text.txt has words left:
-#         histogram()
-#
-#
-#     print new list
-#
+histogram('test.txt')
