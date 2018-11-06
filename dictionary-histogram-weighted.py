3a2fa0d2154d9a26fa0af5de42da2e67f305f661
Erica Naglik <erica2605@live.com>
2018-11-05 23:23:16 -0800

    random word function complete

--- dictionary-histogram-weighted.py
+++ dictionary-histogram-weighted.py
@@ -1,2 +1,22 @@
 import random
 import re
+
+histogram_dictionary = {}
+def histogram(text):
+    for word in text:
+        if word in histogram_dictionary:
+            histogram_dictionary[word] += 1
+        else:
+            histogram_dictionary[word] = 1
+    return histogram_dictionary
+
+def random_word(histogram_dictionary):
+    total = 0
+    words_sum = sum(histogram_dictionary.values())
+    random_sum = random.randint(0, words_sum - 1)
+    for key, value in histogram_dictionary.items():
+        total += value
+        if total > random_sum:
+            return key
+        else:
+            continue
