d432cbc96aeb834b952dc36cefabf58c65fa2cae
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2018-11-21 15:32:18 -0800

    Add files via upload

--- linkedlist.py
+++ linkedlist.py
@@ -1,5 +1,6 @@
 #!python
 
+
 class Node(object):
 
     def __init__(self, data):
@@ -11,19 +12,20 @@ class Node(object):
         """Return a string representation of this node."""
         return 'Node({!r})'.format(self.data)
 
+
 class LinkedList(object):
 
     def __init__(self, items=None):
-        """Initalize this linked list and append the given items, if any"""
-        self.head = None # First node
-        self.tail = None # Last node
+        """Initialize this linked list and append the given items, if any."""
+        self.head = None  # First node
+        self.tail = None  # Last node
         # Append given items
         if items is not None:
             for item in items:
                 self.append(item)
 
     def __str__(self):
-        """Return a formated string representation of this linked list."""
+        """Return a formatted string representation of this linked list."""
         items = ['({!r})'.format(item) for item in self.items()]
         return '[{}]'.format(' -> '.join(items))
 
@@ -35,16 +37,16 @@ class LinkedList(object):
         """Return a list (dynamic array) of all items in this linked list.
         Best and worst case running time: O(n) for n items in the list (length)
         because we always need to loop through all n nodes to get each item."""
-        items = [] # 0(1) time to create empty list
+        items = []  # O(1) time to create empty list
         # Start at head node
-        node = self.head #0(1) time to assign new variable
+        node = self.head  # O(1) time to assign new variable
         # Loop until node is None, which is one node too far past tail
-        while node is not None: #Always n iterations because no early return
-            items.append(node.data) #0(1) time (on average) to append to list
+        while node is not None:  # Always n iterations because no early return
+            items.append(node.data)  # O(1) time (on average) to append to list
             # Skip to next node to advance forward in linked list
-            node = node.next # O(1) time to reassign variable
+            node = node.next  # O(1) time to reassign variable
         # Now list contains items from all nodes
-        return items # 0(1) time to return list
+        return items  # O(1) time to return list
 
     def is_empty(self):
         """Return a boolean indicating whether this linked list is empty."""
@@ -52,28 +54,48 @@ class LinkedList(object):
 
     def length(self):
         """Return the length of this linked list by traversing its nodes.
-        TODO: Running time: 0(???) Why and under what conditions?"""
-        # TODO: Create new node to hold given item
-        # TODO: APpend node after tail, if it exists
+        TODO: Running time: O(???) Why and under what conditions?"""
+        # TODO: Loop through all nodes and count one for each
+        node_count = 0
+        node = self.head
+        while node is not None:
+            node_count += 1
+            node = node.next
+        return node_count
+
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
-        # TODO: Running time: 0(???) Why and under what conditions?"""
+        TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Create new node to hold given item
         # TODO: Append node after tail, if it exists
+        node = new.node
+        if node is not None:
+            self.items.append(item)
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
-        TODO: Running time: 0(???) Why and under what conditions?"""
+        TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Create new node to hold given item
         # TODO: Prepend node before head, if it exists
+        node = new.node
+        if node is not None:
+            items.insert(0, item)
 
     def find(self, quality):
         """Return an item from this linked list satisfying the given quality.
-        TODO: Best case running time: 0(???) Why and under what conditions?
+        TODO: Best case running time: O(???) Why and under what conditions?
         TODO: Worst case running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes to find item where quality(item) is true
+        # TODO: Loop through all nodes to find item where quality(item) is True
         # TODO: Check if node's data satisfies given quality function
+        node = self.head
+        quality = False
+        while node and quality is not True:
+            if quality(node.data) is True:
+                return node.data
+            else:
+                node = node.next
+
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
@@ -84,9 +106,10 @@ class LinkedList(object):
         # TODO: Otherwise raise error to tell user that delete has failed
         # Hint: raise ValueError('Item not found: {}'.format(item))
 
+
 def test_linked_list():
     ll = LinkedList()
-    print('list: {}'.format(11))
+    print('list: {}'.format(ll))
 
     print('\nTesting append:')
     for item in ['A', 'B', 'C']:
@@ -96,7 +119,7 @@ def test_linked_list():
 
     print('head: {}'.format(ll.head))
     print('tail: {}'.format(ll.tail))
-    print('length: {}'.format(ll.lengthh()))
+    print('length: {}'.format(ll.length()))
 
     # Enable this after implementing delete method
     delete_implemented = False
