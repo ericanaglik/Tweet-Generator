dcfcebadbf4d587d8e54f53aca27f7229f99e47e
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2018-11-27 21:26:08 -0800

    Add files via upload

--- hashtable.py
+++ hashtable.py
@@ -39,6 +39,12 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Collect all values in each bucket
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
+
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
@@ -54,12 +60,23 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Count number of key-value entries in each bucket
+        entries = 0
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                entries += 1
+        return entries
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Find bucket where given key belongs
         # TODO: Check if key-value entry exists in bucket
+        index = self._bucket_index(key)
+        finder = False
+        key_value = self.buckets[index].find(lambda item: item[0] == key)
+        if key_value is not None:
+            finder = True
+        return finder
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
@@ -69,6 +86,11 @@ class HashTable(object):
         # TODO: If found, return value associated with given key
         # TODO: Otherwise, raise error to tell user get failed
         # Hint: raise KeyError('Key not found: {}'.format(key))
+        if self.contains(key):
+            bucket = self.buckets[self._bucket_index(key)]
+            return bucket.find(key)
+        else:
+            raise KeyError('Key not found: {}'.format(key))
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
